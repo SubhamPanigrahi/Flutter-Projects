f818658b94e9d13fc6292972eceeee7c93ca4cc9
SubhamPanigrahi <subhamp.official@gmail.com>
2020-05-12 01:23:23 +0530

    FirstProjectCommit

--- FlutterDev/mi_card/lib/main.dart
+++ FlutterDev/mi_card/lib/main.dart
@@ -9,36 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey[900],
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: 100.0,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.yellow,
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+                // backgroundImage: Image(image: AssetImage('images/avatar.png')),
+              ),
+              Container(
+                height: 20,
+              ),
+              Text(
+                'Mikasa Ackerman',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Spaceage',
                 ),
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.green,
+              ),
+              Container(
+                height: 10,
+              ),
+              Text(
+                'Machine of Mass Destruction',
+                style: TextStyle(
+                  color: Colors.blueGrey[400],
+                  fontSize: 25,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.0,
                 ),
-              ],
-            ),
-            Container(
-              width: 100,
-              color: Colors.blue,
-            )
-          ],
+              ),
+            ],
+          ),
         )),
       ),
     );
